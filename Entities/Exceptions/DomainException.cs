18f37572b39db71a1527b15ac1724e7d6e8abb80
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-18 18:59:29 -0300

    first commit

---
+++ Entities/Exceptions/DomainException.cs
@@ -0,0 +1,7 @@
+﻿namespace Exceptions3.Entities.Exceptions
+{
+    internal class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message) { }
+    }
+}
